788e105b68ab7bd1b2b15695098afb03396fcd70
Amrathesh <amrathesh@arm.com>
2023-09-19 12:21:51 +0530

    Added features to ethtool-test.py
    - added router discovery logic
    - ping to router and arm.com
    - is dhcp enabled check
    - added 8.8.8.8 DNS server to resolv.conf
    
    Signed-off-by: Amrathesh <amrathesh@arm.com>

--- IR/Yocto/meta-woden/recipes-acs/install-files/files/ethtool-test.py
+++ IR/Yocto/meta-woden/recipes-acs/install-files/files/ethtool-test.py
@@ -18,6 +18,7 @@
 # self-test if the interface supports.
 
 import subprocess
+import re
 
 def print_color(text, color="default"):
     colors = {
@@ -69,24 +70,90 @@ if __name__ == "__main__":
             # Dump ethtool prints for each ethernet interface reported
             print_color(f"INFO: Running \"ethtool {intrf} \" :", "green")
             command = f"ethtool {intrf}"
-            result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=False)
-            print(result.stdout)
-            print(result.stderr)
+            result_ethdump = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=False)
+            print(result_ethdump.stdout)
+            print(result_ethdump.stderr)
 
             # Run ethernet self-test if the drivers supports it
             command = f"ethtool -i {intrf}"
-            result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=False)
-            if "supports-test: yes" in result.stdout:
+            result_test = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=False)
+            if "supports-test: yes" in result_test.stdout:
                 print_color(f"INFO: Ethernet interface {intrf} supports ethtool self test.", "green")
                 command = f"ethtool -t {intrf}"
                 print_color(f"INFO: Running {command} :", "green")
-                result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=False)
-                print(result.stdout)
-                print(result.stderr)
+                result_test = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=False)
+                print(result_test.stdout)
+                print(result_test.stderr)
             else:
                 print_color(f"INFO: Ethernet interface {intrf} doesn't supports ethtool self test", "green")
+
+            # don't continue testing if link not detected using ethtool
+            if "Link detected: yes" not in result_ethdump.stdout:
+                print_color(f"INFO: Link not detected for {intrf}", "red")
+                continue
+            else:
+                print_color(f"INFO: Link detected on {intrf}", "green")
+
+            # check if DHCP enabled for interface, else skip testing
+            command = f"ip address show dev {intrf}"
+            result_dhcp = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=False)
+            print_color(f"INFO: Running {command} :", "green")
+            print(result_dhcp.stdout)
+            print(result_dhcp.stderr)
+            if "dynamic" not in result_dhcp.stdout:
+                print_color(f"INFO: {intrf} doesn't support DHCP", "red")
+                continue
+            else:
+                print_color(f"INFO: {intrf} support DHCP", "green")
+
+            # find router/gateway IP and ping it
+            command = f"ip route show dev {intrf}"
+            result_router = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=False)
+            print_color(f"INFO: Running {command} :", "green")
+            print(result_router.stdout)
+            print(result_router.stderr)
+
+            ip_pattern = r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
+            router_ip = re.search(ip_pattern, result_router.stdout)
+
+            if router_ip:
+                ip_address = router_ip.group()
+                print_color(f"INFO: Router/Gateway IP for {intrf} : {ip_address}", "green")
+            else:
+                print_color(f"INFO: Unable to find Router/Gateway IP for {intrf}", "red")
+                continue
+
+            command = f"ping -w 1000 -c 3 -I {intrf} {ip_address}"
+            print_color(f"INFO: Running {command} :", "green")
+            result_ping = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=False)
+            print(result_ping.stdout)
+            print(result_ping.stderr)
+
+            # skip other tests if ping doesn't work
+            if result_ping.returncode != 0 and "100% packet loss" in result_ping.stdout:
+                print_color(f"INFO: Failed to ping router/gateway[{ip_address}] for {intrf}", "red")
+                continue
+            else:
+                print_color(f"INFO: Ping to router/gateway[{ip_address}] for {intrf} is successful", "green")
+
+            # ping www.arm.com to check whether DNS is working
+            command = f"ping -w 1000 -c 3 -I {intrf} www.arm.com"
+            print_color(f"INFO: Running {command} :", "green")
+            result_ping = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=False)
+            print(result_ping.stdout)
+            print(result_ping.stderr)
+
+            if "bad address" in result_ping.stderr:
+                print_color(f"INFO: Unable to resolve www.arm.com, DNS not configured correctly for {intrf}",)
+
+            if result_ping.returncode != 0 and "100% packet loss" in result_ping.stdout:
+                print_color(f"INFO: Failed to ping www.arm.com via {intrf}", "red")
+            else:
+                print_color(f"INFO: Ping to www.arm.com is successful", "green")
             print("\n****************************************************************\n")
         exit(0)
     except Exception as e:
         print_color(f"Error occurred: {e}", "red")
         exit(1)
+
+
