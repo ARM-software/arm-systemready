f9ab49434634059e08dc7b4b462cd66f588cbeab
chetan singh rathore <chetanrgithub@gmail.com>
2025-04-09 10:03:14 +0530

    Direct the output to FWTSResults.log when fwts is run as part of execution enviroment

--- common/linux_scripts/init.sh
+++ common/linux_scripts/init.sh
@@ -162,7 +162,7 @@ if [ $ADDITIONAL_CMD_OPTION != "noacs" ]; then
     if [ "$automation_enabled" == "False" ]; then
       fwts  -r stdout -q --uefi-set-var-multiple=1 --uefi-get-mn-count-multiple=1 --sbbr esrt uefibootpath aest cedt slit srat hmat pcct pdtt bgrt bert einj erst hest sdei nfit iort mpam ibft ras2 >> /mnt/acs_results/fwts/FWTSResults.log
     else
-      $fwts_command >> /mnt/acs_results/fwts/FWTSResults.log
+      $fwts_command -r stdout -q >> /mnt/acs_results/fwts/FWTSResults.log
     fi
     sync /mnt
     sleep 5
