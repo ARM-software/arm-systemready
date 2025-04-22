09b70a1ce48988e9dd9f8d9ef35aa0b2c2aadf90
chetan singh rathore <chetanrgithub@gmail.com>
2025-04-22 18:06:47 +0530

    SR readme and minor changes (#386)
    * echo acs pdf path inside yocto flag
    
    * SR Readme updates
    
    * Update systemready-band-source.cfg with acs test tools 3.0.1 tags
    
    * Update bbsr_startup.nsh : optimize the parser logic

--- common/log_parser/main_log_parser.sh
+++ common/log_parser/main_log_parser.sh
@@ -781,8 +781,8 @@ if [ $YOCTO_FLAG_PRESENT -eq 1 ]; then
     # Convert ACS Summary HTML to PDF
     if [ -f "$ACS_SUMMARY_HTML" ]; then
         python3 -c "from weasyprint import HTML, CSS; HTML('$ACS_SUMMARY_HTML').write_pdf('$ACS_SUMMARY_PDF', stylesheets=[CSS(string='@page { margin: 0; }')])"
+        echo "ACS PDF Summary : $ACS_SUMMARY_PDF"
     fi
 fi
-echo "ACS PDF Summary : $ACS_SUMMARY_PDF"
 
 echo ""
