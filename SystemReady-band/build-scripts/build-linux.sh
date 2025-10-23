1a2bfdc3859e0fcb4794342c81df4ea73b0c5973
Guru9902 <gururaj.revankar@arm.com>
2025-10-23 06:19:14 +0000

    Update linux kernel version to v6.16
    - xhci-pci driver is build as part of kernel
    - Yocto recipe for linux version 6.16 added
    - 0001-SystemReady-Linux-6.16.patch added
    - increase INITRAMFS_MAXSIZE due to Linux update
    
    Signed-off-by: Guru9902 <gururaj.revankar@arm.com>
    Change-Id: I9ec6e1643ab7c89fbed46ecf8e40d110270c90e1

--- SystemReady-band/build-scripts/build-linux.sh
+++ SystemReady-band/build-scripts/build-linux.sh
@@ -87,7 +87,6 @@ do_build ()
     echo "CONFIG_SERIAL_8250_NR_UARTS=32" >> $LINUX_OUT_DIR/.config
     echo "CONFIG_SERIAL_8250_RUNTIME_UARTS=32" >> $LINUX_OUT_DIR/.config
     cat $SRBAND_DEFCONFIG >> $LINUX_OUT_DIR/.config
-
     if [[ $arch = "aarch64" ]]
     then
         echo "arm64 machine"
@@ -126,7 +125,6 @@ do_package ()
     cp $TOP_DIR/$LINUX_PATH/$LINUX_OUT_DIR/drivers/nvme/host/nvme.ko $TOP_DIR/ramdisk/drivers
     cp $TOP_DIR/$LINUX_PATH/$LINUX_OUT_DIR/drivers/nvme/host/nvme-core.ko $TOP_DIR/ramdisk/drivers
     cp $TOP_DIR/$LINUX_PATH/$LINUX_OUT_DIR/drivers/usb/host/xhci-pci-renesas.ko $TOP_DIR/ramdisk/drivers
-    cp $TOP_DIR/$LINUX_PATH/$LINUX_OUT_DIR/drivers/usb/host/xhci-pci.ko $TOP_DIR/ramdisk/drivers
     cp $TOP_DIR/$LINUX_PATH/$LINUX_OUT_DIR/drivers/char/tpm/tpm_tis.ko $TOP_DIR/ramdisk/drivers
     cp $TOP_DIR/$LINUX_PATH/$LINUX_OUT_DIR/drivers/char/tpm/tpm_tis_spi.ko $TOP_DIR/ramdisk/drivers
     cp $TOP_DIR/$LINUX_PATH/$LINUX_OUT_DIR/drivers/char/tpm/tpm_tis_i2c_cr50.ko $TOP_DIR/ramdisk/drivers
