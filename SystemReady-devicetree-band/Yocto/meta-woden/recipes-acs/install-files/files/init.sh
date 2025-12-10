cf7a4f5170e5772e4b58de716a7a1c40094326d7
chetan singh rathore <chetan.singh2@arm.com>
2025-12-10 22:46:40 +0530

    Invoking network boot before capsule update

--- SystemReady-devicetree-band/Yocto/meta-woden/recipes-acs/install-files/files/init.sh
+++ SystemReady-devicetree-band/Yocto/meta-woden/recipes-acs/install-files/files/init.sh
@@ -95,7 +95,7 @@ if [ $ADDITIONAL_CMD_OPTION != "noacs" ]; then
     fi
 
     check_flag=0
-    if [ -f /mnt/acs_tests/app/capsule_update_done.flag ] || [ -f /mnt/acs_tests/app/capsule_update_ignore.flag ] || [ -f /mnt/acs_tests/app/capsule_update_unsupport.flag ] || [ -f /mnt/acs_tests/app/linux_run_complete.flag ]; then
+    if [ -f /mnt/acs_tests/app/capsule_update_done.flag ] || [ -f /mnt/acs_tests/app/capsule_update_ignore.flag ] || [ -f /mnt/acs_tests/app/capsule_update_unsupport.flag ]; then
       check_flag=1
     fi
 
@@ -108,180 +108,191 @@ if [ $ADDITIONAL_CMD_OPTION != "noacs" ]; then
       else
         echo "Failed to create capsule update check flag"
       fi
-      touch /mnt/acs_tests/app/linux_run_complete.flag
 
-
-      #LINUX DEBUG DUMP
-      echo "Collecting Linux debug logs"
-      LINUX_DUMP_DIR="/mnt/acs_results_template/acs_results/linux_dump"
-      mkdir -p $LINUX_DUMP_DIR
-      echo 1 > /proc/sys/kernel/printk
-      timedatectl set-ntp true &> $LINUX_DUMP_DIR/set_ntp_time.log
-      timedatectl &> $LINUX_DUMP_DIR/timedatectl.log
-      echo 7 > /proc/sys/kernel/printk
-      lspci -vvv &> $LINUX_DUMP_DIR/lspci.log
-      lsusb    > $LINUX_DUMP_DIR/lsusb.log
-      uname -a > $LINUX_DUMP_DIR/uname.log
-      dmesg > $LINUX_DUMP_DIR/dmesg.log
-      journalctl > $LINUX_DUMP_DIR/journalctl.log
-      cat /proc/interrupts > $LINUX_DUMP_DIR/interrupts.log
-      cat /proc/cpuinfo    > $LINUX_DUMP_DIR/cpuinfo.log
-      cat /proc/meminfo    > $LINUX_DUMP_DIR/meminfo.log
-      cat /proc/iomem      > $LINUX_DUMP_DIR/iomem.log
-      ls -lR /sys/firmware > $LINUX_DUMP_DIR/firmware.log
-      cp -r /sys/firmware $LINUX_DUMP_DIR/
-      dmidecode  > $LINUX_DUMP_DIR/dmidecode.log
-      efibootmgr > $LINUX_DUMP_DIR/efibootmgr.log
-      fwupdmgr get-devices          &> $LINUX_DUMP_DIR/fwupd_getdevices.log
-      echo "0" | fwupdtool esp-list &> $LINUX_DUMP_DIR/fwupd_esplist.log
-      fwupdmgr get-bios-settings    &> $LINUX_DUMP_DIR/fwupd_bios_setting.log
-      fwupdmgr get-history          &> $LINUX_DUMP_DIR/fwupd_get_history.log
-      sync /mnt
-      sleep 5
-      echo "Linux debug logs run - Completed"
-
-
-      # FWTS EBBR run
-      mkdir -p /mnt/acs_results_template/acs_results/fwts
-      if [ -f /lib/modules/*/kernel/smccc_test/smccc_test.ko ]; then
-        echo "Loading FWTS SMCCC module"
-        insmod /lib/modules/*/kernel/smccc_test/smccc_test.ko
-      else
-        echo "Error: FWTS SMCCC kernel Driver is not found."
-      fi
-      echo "Executing FWTS for EBBR"
-      test_list=`cat /usr/bin/ir_bbr_fwts_tests.ini | grep -v "^#" | awk '{print $1}' | xargs`
-      echo "Test Executed are $test_list"
-      echo "SystemReady devicetree band ACS v3.1.1 (RC-Final)" > /mnt/acs_results_template/acs_results/fwts/FWTSResults.log
-      /usr/bin/fwts --ebbr `echo $test_list` smccc -r stdout >> /mnt/acs_results_template/acs_results/fwts/FWTSResults.log
-      echo -e -n "\n"
-      sync /mnt
-      sleep 5
-      echo "FWTS test execution - Completed"
-
-
-      #LINUX BSA RUN
-      mkdir -p /mnt/acs_results_template/acs_results/linux_acs/bsa_acs_app
-      if [ -f /lib/modules/*/kernel/bsa_acs/bsa_acs.ko ]; then
-        echo "Running Linux BSA tests"
-        insmod /lib/modules/*/kernel/bsa_acs/bsa_acs.ko
-        echo "SystemReady devicetree band ACS v3.1.1 (RC-Final)" > /mnt/acs_results_template/acs_results/linux_acs/bsa_acs_app/BSALinuxResults.log
-        bsa --skip-dp-nic-ms >> /mnt/acs_results_template/acs_results/linux_acs/bsa_acs_app/BSALinuxResults.log
-        dmesg | sed -n 'H; /PE_INFO/h; ${g;p;}' > /mnt/acs_results_template/acs_results/linux_acs/bsa_acs_app/BsaResultsKernel.log
+      if [ ! -f /mnt/acs_tests/app/linux_run_complete.flag ]; then
+        touch /mnt/acs_tests/app/linux_run_complete.flag
+      
+        #LINUX DEBUG DUMP
+        echo "Collecting Linux debug logs"
+        LINUX_DUMP_DIR="/mnt/acs_results_template/acs_results/linux_dump"
+        mkdir -p $LINUX_DUMP_DIR
+        echo 1 > /proc/sys/kernel/printk
+        timedatectl set-ntp true &> $LINUX_DUMP_DIR/set_ntp_time.log
+        timedatectl &> $LINUX_DUMP_DIR/timedatectl.log
+        echo 7 > /proc/sys/kernel/printk
+        lspci -vvv &> $LINUX_DUMP_DIR/lspci.log
+        lsusb    > $LINUX_DUMP_DIR/lsusb.log
+        uname -a > $LINUX_DUMP_DIR/uname.log
+        dmesg > $LINUX_DUMP_DIR/dmesg.log
+        journalctl > $LINUX_DUMP_DIR/journalctl.log
+        cat /proc/interrupts > $LINUX_DUMP_DIR/interrupts.log
+        cat /proc/cpuinfo    > $LINUX_DUMP_DIR/cpuinfo.log
+        cat /proc/meminfo    > $LINUX_DUMP_DIR/meminfo.log
+        cat /proc/iomem      > $LINUX_DUMP_DIR/iomem.log
+        ls -lR /sys/firmware > $LINUX_DUMP_DIR/firmware.log
+        cp -r /sys/firmware $LINUX_DUMP_DIR/
+        dmidecode  > $LINUX_DUMP_DIR/dmidecode.log
+        efibootmgr > $LINUX_DUMP_DIR/efibootmgr.log
+        fwupdmgr get-devices          &> $LINUX_DUMP_DIR/fwupd_getdevices.log
+        echo "0" | fwupdtool esp-list &> $LINUX_DUMP_DIR/fwupd_esplist.log
+        fwupdmgr get-bios-settings    &> $LINUX_DUMP_DIR/fwupd_bios_setting.log
+        fwupdmgr get-history          &> $LINUX_DUMP_DIR/fwupd_get_history.log
         sync /mnt
         sleep 5
-        echo "Linux BSA test execution - Completed"
-      else
-        echo "Error: BSA kernel Driver is not found. Linux BSA tests cannot be run"
-      fi
+        echo "Linux debug logs run - Completed"
 
 
-      # Device Driver Info script
-      mkdir -p /home/root/fdt
-      mkdir -p /mnt/acs_results_template/acs_results/linux_tools
-      pushd /usr/bin
-      echo "running device_driver_info.sh device and driver info created"
-      ./device_driver_info.sh
-      cp device_driver_info.log /mnt/acs_results_template/acs_results/linux_tools
-      echo "device driver script run completed"
-      popd
-      sync /mnt
-      sleep 5
-
-
-      # DT VALIDATE RUN
-      # Generate the .dts file and move it to /mnt/acs_results_template/acs_results/linux_tools
-      dtc -I fs -O dts -o /mnt/acs_results_template/acs_results/linux_tools/device_tree.dts /sys/firmware/devicetree/base 2>/dev/null
-      # Generate tree format of sys hierarchy and saving it into logs.
-      tree -d /sys > /mnt/acs_results_template/acs_results/linux_dump/sys_hierarchy.log
-      if [ -f /sys/firmware/fdt ]; then
-        echo "copying fdt "
-        cp /sys/firmware/fdt /home/root/fdt
-        sync /mnt
-
-        # Device Tree Validate script
-        if [ -f /mnt/acs_results_template/acs_results/linux_tools/dt-validate.log ]; then
-          mv /mnt/acs_results_template/acs_results/linux_tools/dt-validate.log /mnt/acs_results_template/acs_results/linux_tools/dt-validate.log.old
+        # FWTS EBBR run
+        mkdir -p /mnt/acs_results_template/acs_results/fwts
+        if [ -f /lib/modules/*/kernel/smccc_test/smccc_test.ko ]; then
+          echo "Loading FWTS SMCCC module"
+          insmod /lib/modules/*/kernel/smccc_test/smccc_test.ko
+        else
+          echo "Error: FWTS SMCCC kernel Driver is not found."
         fi
-        echo "Running dt-validate tool "
-        dt-validate -s /usr/bin/processed_schema.json -m /home/root/fdt/fdt 2>> /mnt/acs_results_template/acs_results/linux_tools/dt-validate.log
-        if [ ! -s /mnt/acs_results_template/acs_results/linux_tools/dt-validate.log ]; then
-          echo "The FDT is compliant according to schema " >> /mnt/acs_results_template/acs_results/linux_tools/dt-validate.log
-        fi
-        sed -i '1s/^/SystemReady devicetree band ACS v3.1.1 (RC-Final) \nDeviceTree bindings of Linux kernel version: 6.18 \ndtschema version: 2025.02 \n\n/' /mnt/acs_results_template/acs_results/linux_tools/dt-validate.log
-        # Run dt parser on dt-validate log to categorize failures
-        /usr/bin/systemready-scripts/dt-parser.py /mnt/acs_results_template/acs_results/linux_tools/dt-validate.log --print 2>&1 | tee /mnt/acs_results_template/acs_results/linux_tools/dt-validate-parser.log
-      else
-        echo  "Error: The FDT devicetree file, fdt, does not exist at /sys/firmware/fdt. Cannot run dt-schema tool" | tee /mnt/acs_results_template/acs_results/linux_tools/dt-validate.log
-      fi
-      sync /mnt
-      sleep 5
-
-
-      # Capturing System PSCI command output
-      echo "Collecting psci command output"
-      mkdir -p /mnt/acs_results_template/acs_results/linux_tools/psci
-      mount -t debugfs none /sys/kernel/debug
-      cat /sys/kernel/debug/psci > /mnt/acs_results_template/acs_results/linux_tools/psci/psci.log
-      dmesg | grep psci > /mnt/acs_results_template/acs_results/linux_tools/psci/psci_kernel.log
-      sync /mnt
-      sleep 5
-      echo "PSCI command output - Completed"
-
-
-      # DT Kernel Self test run
-      echo "Running DT Kernel Self Test"
-      pushd /usr/kernel-selftest
-      chmod +x dt/test_unprobed_devices.sh
-      chmod +x kselftest/ktap_helpers.sh
-      ./run_kselftest.sh -t dt:test_unprobed_devices.sh > /mnt/acs_results_template/acs_results/linux_tools/dt_kselftest.log
-      popd
-      sync /mnt
-      sleep 5
-      echo "DT Kernel Self test run - Completed"
-
-
-      # ETHTOOL test run
-      echo "Running Ethtool test"
-      # update resolv.conf with 8.8.8.8 DNS server
-      echo "nameserver 8.8.8.8" >> /etc/resolv.conf
-      # run ethtool-test.py, dump ethernet information, run self-tests if supported, and ping
-      python3 /bin/ethtool-test.py /mnt/acs_tests/config/system_config.txt | tee ethtool-test.log
-      # remove color characters from log and save
-      awk '{gsub(/\x1B\[[0-9;]*[JKmsu]/, "")}1' ethtool-test.log > /mnt/acs_results_template/acs_results/linux_tools/ethtool-test.log
-      sync /mnt
-      sleep 5
-      echo "Ethtool test run - Completed"
-
-      # READ_WRITE_BLOCK_DEVICE run
-      # RUN read_write_check_blk_devices.py, parse block devices, and perform read if partition doesn't belond in precious partitions
-      echo "Running BLK devices read and write check"
-      python3 /bin/read_write_check_blk_devices.py | tee /mnt/acs_results_template/acs_results/linux_tools/read_write_check_blk_devices.log
-      sync /mnt
-      sleep 5
-      echo "BLK devices read and write check - Completed"
-
-
-      # systemready-scripts running
-      if [ -d "/mnt/acs_results_template" ]; then
-        echo "Running post scripts check "
-        cd /mnt/acs_results_template
-        mkdir -p /mnt/acs_results_template/acs_results/post-script
-        # Create .stamp file for avoding cache regen
-        echo "linux-6.18" > /usr/bin/linux-6.18/.stamp
-        echo "  Generating comatible strings"
-        /usr/bin/systemready-scripts/compatibles /usr/bin/linux-6.18/bindings > /usr/bin/linux-6.18/compatible-strings.txt
+        echo "Executing FWTS for EBBR"
+        test_list=`cat /usr/bin/ir_bbr_fwts_tests.ini | grep -v "^#" | awk '{print $1}' | xargs`
+        echo "Test Executed are $test_list"
+        echo "SystemReady devicetree band ACS v3.1.1 (RC-Final)" > /mnt/acs_results_template/acs_results/fwts/FWTSResults.log
+        /usr/bin/fwts --ebbr `echo $test_list` smccc -r stdout >> /mnt/acs_results_template/acs_results/fwts/FWTSResults.log
+        echo -e -n "\n"
         sync /mnt
         sleep 5
-        echo "  Generating comatible strings - Completed"
-        /usr/bin/systemready-scripts/check-sr-results.py --cache-dir /usr/bin \
-         --linux-url https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.18.tar.xz --dir /mnt/acs_results_template 2>&1 | tee /mnt/acs_results_template/acs_results/post-script/post-script.log
-        cd -
-      fi
-      sync /mnt
-      sleep 5
-      echo "Post scripts check - Completed"
+        echo "FWTS test execution - Completed"
 
+
+        #LINUX BSA RUN
+        mkdir -p /mnt/acs_results_template/acs_results/linux_acs/bsa_acs_app
+        if [ -f /lib/modules/*/kernel/bsa_acs/bsa_acs.ko ]; then
+          echo "Running Linux BSA tests"
+          insmod /lib/modules/*/kernel/bsa_acs/bsa_acs.ko
+          echo "SystemReady devicetree band ACS v3.1.1 (RC-Final)" > /mnt/acs_results_template/acs_results/linux_acs/bsa_acs_app/BSALinuxResults.log
+          bsa --skip-dp-nic-ms >> /mnt/acs_results_template/acs_results/linux_acs/bsa_acs_app/BSALinuxResults.log
+          dmesg | sed -n 'H; /PE_INFO/h; ${g;p;}' > /mnt/acs_results_template/acs_results/linux_acs/bsa_acs_app/BsaResultsKernel.log
+          sync /mnt
+          sleep 5
+          echo "Linux BSA test execution - Completed"
+        else
+          echo "Error: BSA kernel Driver is not found. Linux BSA tests cannot be run"
+        fi
+
+
+        # Device Driver Info script
+        mkdir -p /home/root/fdt
+        mkdir -p /mnt/acs_results_template/acs_results/linux_tools
+        pushd /usr/bin
+        echo "running device_driver_info.sh device and driver info created"
+        ./device_driver_info.sh
+        cp device_driver_info.log /mnt/acs_results_template/acs_results/linux_tools
+        echo "device driver script run completed"
+        popd
+        sync /mnt
+        sleep 5
+
+
+        # DT VALIDATE RUN
+        # Generate the .dts file and move it to /mnt/acs_results_template/acs_results/linux_tools
+        dtc -I fs -O dts -o /mnt/acs_results_template/acs_results/linux_tools/device_tree.dts /sys/firmware/devicetree/base 2>/dev/null
+        # Generate tree format of sys hierarchy and saving it into logs.
+        tree -d /sys > /mnt/acs_results_template/acs_results/linux_dump/sys_hierarchy.log
+        if [ -f /sys/firmware/fdt ]; then
+          echo "copying fdt "
+          cp /sys/firmware/fdt /home/root/fdt
+          sync /mnt
+
+          # Device Tree Validate script
+          if [ -f /mnt/acs_results_template/acs_results/linux_tools/dt-validate.log ]; then
+            mv /mnt/acs_results_template/acs_results/linux_tools/dt-validate.log /mnt/acs_results_template/acs_results/linux_tools/dt-validate.log.old
+          fi
+          echo "Running dt-validate tool "
+          dt-validate -s /usr/bin/processed_schema.json -m /home/root/fdt/fdt 2>> /mnt/acs_results_template/acs_results/linux_tools/dt-validate.log
+          if [ ! -s /mnt/acs_results_template/acs_results/linux_tools/dt-validate.log ]; then
+            echo "The FDT is compliant according to schema " >> /mnt/acs_results_template/acs_results/linux_tools/dt-validate.log
+          fi
+          sed -i '1s/^/SystemReady devicetree band ACS v3.1.1 (RC-Final) \nDeviceTree bindings of Linux kernel version: 6.18 \ndtschema version: 2025.02 \n\n/' /mnt/acs_results_template/acs_results/linux_tools/dt-validate.log
+          # Run dt parser on dt-validate log to categorize failures
+          /usr/bin/systemready-scripts/dt-parser.py /mnt/acs_results_template/acs_results/linux_tools/dt-validate.log --print 2>&1 | tee /mnt/acs_results_template/acs_results/linux_tools/dt-validate-parser.log
+        else
+          echo  "Error: The FDT devicetree file, fdt, does not exist at /sys/firmware/fdt. Cannot run dt-schema tool" | tee /mnt/acs_results_template/acs_results/linux_tools/dt-validate.log
+        fi
+        sync /mnt
+        sleep 5
+
+
+        # Capturing System PSCI command output
+        echo "Collecting psci command output"
+        mkdir -p /mnt/acs_results_template/acs_results/linux_tools/psci
+        mount -t debugfs none /sys/kernel/debug
+        cat /sys/kernel/debug/psci > /mnt/acs_results_template/acs_results/linux_tools/psci/psci.log
+        dmesg | grep psci > /mnt/acs_results_template/acs_results/linux_tools/psci/psci_kernel.log
+        sync /mnt
+        sleep 5
+        echo "PSCI command output - Completed"
+
+
+        # DT Kernel Self test run
+        echo "Running DT Kernel Self Test"
+        pushd /usr/kernel-selftest
+        chmod +x dt/test_unprobed_devices.sh
+        chmod +x kselftest/ktap_helpers.sh
+        ./run_kselftest.sh -t dt:test_unprobed_devices.sh > /mnt/acs_results_template/acs_results/linux_tools/dt_kselftest.log
+        popd
+        sync /mnt
+        sleep 5
+        echo "DT Kernel Self test run - Completed"
+
+
+        # ETHTOOL test run
+        echo "Running Ethtool test"
+        # update resolv.conf with 8.8.8.8 DNS server
+        echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+        # run ethtool-test.py, dump ethernet information, run self-tests if supported, and ping
+        python3 /bin/ethtool-test.py /mnt/acs_tests/config/system_config.txt | tee ethtool-test.log
+        # remove color characters from log and save
+        awk '{gsub(/\x1B\[[0-9;]*[JKmsu]/, "")}1' ethtool-test.log > /mnt/acs_results_template/acs_results/linux_tools/ethtool-test.log
+        sync /mnt
+        sleep 5
+        echo "Ethtool test run - Completed"
+
+        # READ_WRITE_BLOCK_DEVICE run
+        # RUN read_write_check_blk_devices.py, parse block devices, and perform read if partition doesn't belond in precious partitions
+        echo "Running BLK devices read and write check"
+        python3 /bin/read_write_check_blk_devices.py | tee /mnt/acs_results_template/acs_results/linux_tools/read_write_check_blk_devices.log
+        sync /mnt
+        sleep 5
+        echo "BLK devices read and write check - Completed"
+
+
+        # SYSTEMREADY-SCRIPTS running
+        if [ -d "/mnt/acs_results_template" ]; then
+          echo "Running post scripts check "
+          cd /mnt/acs_results_template
+          mkdir -p /mnt/acs_results_template/acs_results/post-script
+          # Create .stamp file for avoding cache regen
+          echo "linux-6.18" > /usr/bin/linux-6.18/.stamp
+          echo "  Generating comatible strings"
+          /usr/bin/systemready-scripts/compatibles /usr/bin/linux-6.18/bindings > /usr/bin/linux-6.18/compatible-strings.txt
+          sync /mnt
+          sleep 5
+          echo "  Generating comatible strings - Completed"
+          /usr/bin/systemready-scripts/check-sr-results.py --cache-dir /usr/bin \
+           --linux-url https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.18.tar.xz --dir /mnt/acs_results_template 2>&1 | tee /mnt/acs_results_template/acs_results/post-script/post-script.log
+          cd -
+        fi
+        sync /mnt
+        sleep 10
+        echo "Post scripts check - Completed"
+
+
+        # NETWORK BOOT script
+        pushd /usr/bin
+        echo "running network boot script"
+        ./acs_https_network_boot.sh
+        echo "network script run completed"
+        popd
+        sync /mnt
+        sleep 5
+      fi      
       if [ $capsule_update_check -eq 1 ]; then
         umount /mnt
         sleep 5
@@ -365,15 +376,6 @@ if [ $ADDITIONAL_CMD_OPTION != "noacs" ]; then
         rm /mnt/acs_tests/app/capsule_update_ignore.flag
       fi
 
-      #Network Boot script
-      mkdir -p /home/root/fdt
-      pushd /usr/bin
-      echo "running network boot script"
-      ./acs_https_network_boot.sh
-      echo "network script run completed"
-      popd
-      sync /mnt
-      sleep 5
 
       # EDK2 Parser Tool run
       if [ -d "/mnt/acs_results_template/acs_results/sct_results" ]; then
