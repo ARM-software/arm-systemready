349387de4f837fe1743272c7d1cead4ee93318e0
Manjunatha D <manjunatha.d@arm.com>
2026-03-26 12:31:23 +0000

    fix: move S_L3_01 skip rule from scripts to acs_run_config.ini
    - Removes UEFI hardcoded --skip to avoid overlapping
    - Update Linux and UEFI scripts to match the handling
    
    Signed-off-by: Manjunatha D <manjunatha.d@arm.com>
    Change-Id: Ib1bbe5c698fa00d4fbf9c3a9f4997f3e211a5b26

--- common/linux_scripts/init.sh
+++ common/linux_scripts/init.sh
@@ -246,7 +246,6 @@ if [ $ADDITIONAL_CMD_OPTION != "noacs" ]; then
     fi
   fi
 
-
   # Linux SBSA Execution
   echo "Running Linux SBSA tests"
   if [ "$automation_enabled" == "True" ]; then
@@ -257,8 +256,8 @@ if [ $ADDITIONAL_CMD_OPTION != "noacs" ]; then
       if [ -f  /lib/modules/sbsa_acs.ko ]; then
         insmod /lib/modules/sbsa_acs.ko
         echo "${SR_VERSION}" > /mnt/acs_results/linux/SbsaResultsApp.log
-        echo "Running command $sbsa_command --skip S_L3_01,PCI_MM_03 --skip-dp-nic-ms"
-        $sbsa_command --skip S_L3_01,PCI_MM_03 --skip-dp-nic-ms >> /mnt/acs_results/linux/SbsaResultsApp.log
+        echo "Running command $sbsa_command --skip PCI_MM_03 --skip-dp-nic-ms"
+        $sbsa_command --skip PCI_MM_03 --skip-dp-nic-ms >> /mnt/acs_results/linux/SbsaResultsApp.log
         dmesg | sed -n 'H; /PE_INFO/h; ${g;p;}' > /mnt/acs_results/linux/SbsaResultsKernel.log
         sync /mnt
         sleep 5
